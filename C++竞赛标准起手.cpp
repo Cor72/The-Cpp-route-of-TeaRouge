17142a65d5b6ce249014fa2a29aba4c067a32a3e
TeaRouge <Qiguo72@outlook.com>
2025-09-12 08:57:13 +0800

    vs select

---
+++ C++竞赛标准起手.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+
+void solve()
+{
+    
+    
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int _;cin>>_;
+    while(_--)solve();
+    return 0;
+}
