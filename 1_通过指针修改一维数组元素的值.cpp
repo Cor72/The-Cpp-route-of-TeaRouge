e06e59c98becd8b156f601bf7f80362a2aaab5c0
Cor72 <Qiguo72@outlook.com>
2026-01-03 22:00:27 +0800

    添加了一个链表小项目

---
+++ 1_通过指针修改一维数组元素的值.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+
+void solve()
+{
+    int count;
+    cin>>count;
+    vector<int> a(count);
+    for(int i=0;i<count;i++)
+    {
+        cin>>a[i];
+    }
+
+
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T;cin>>T;
+    while(T--)
+    solve();
+    return 0;
+}
---
+++ 5_数组循环右移.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+
+void solve()
+{
+    int count;
+
+    for (int i = 0; i < count; i++)
+    {                                     
+
+        cin>>a[i];
+    }
+    for (int i = 0; i < count; i++)
+    {
+        
+    }
+    
+    
+
+
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T;cin>>T;
+    while(T--)
+    solve();
+    return 0;
+}
